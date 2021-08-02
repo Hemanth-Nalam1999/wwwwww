8662cbcd187dd8e95e8fe241382e69a78b549341
Hemanth-Nalam <nalamhemanth1999@.com>
2021-08-02 11:22:36 +0530

    from

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # doing necessary imports
 
 from flask import Flask, render_template, request,jsonify
-# from flask_cors import CORS,cross_origin
+from flask_cors import CORS,cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
